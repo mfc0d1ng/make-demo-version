8190de3874c37bcb689401fbad952e10c13854a4
Mohamed fareed <131618380+mfc0d1ng@users.noreply.github.com>
2023-06-05 11:32:31 +0300

    Update main.cpp

--- make/main.cpp
+++ make/main.cpp
@@ -197,7 +197,7 @@ int main(int argc, char *argv[])
             {
                 if(content[i] == '\n')
                 {
-                    cmd.push_back(' '); 
+                    ++i;
                 }
 		else
 		{
