dccd4de02a4b9bcbb6b65875edfab62ff8395a60
Mohamed fareed <131618380+mfc0d1ng@users.noreply.github.com>
2023-06-05 12:21:52 +0300

    Update main.cpp

--- make/main.cpp
+++ make/main.cpp
@@ -172,7 +172,7 @@ int main(int argc, char *argv[])
         
 	    if(content[i-2] != ':')
             {
-                std::printf("Error: expected colon ':' at line %i where '%c' is found ...\n%s\n", ++line, content[i-2],
+                std::printf("Error: expected colon ':' where '%c' is found (line %i) ...\n%s\n", content[i-2], ++line,
                             "The basic textual structure of map.txt can't be changed.\n" 
                             "Fix this problem or remove the current map.txt and run the program again\n"
                             "in order to let the program to create A new map.txt in the program directory.\n"
@@ -183,7 +183,7 @@ int main(int argc, char *argv[])
 
             if(content[i-1] != '"')
             {
-                std::printf("Error: expected double quote '\"' at line %i where '%c' is found ...\n%s\n", ++line, content[i-1],
+                std::printf("Error: expected double quote '\"' where '%c' is found (line %i) ...\n%s\n", content[i-1], ++line,
                             "The basic textual structure of map.txt can't be changed.\n"
                             "Fix this problem or remove the current map.txt and run the program again\n"
                             "in order to let the program to create A new map.txt in the program directory.\n"
