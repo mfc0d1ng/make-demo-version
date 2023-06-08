d7687157c9efd74d959dcc2cf7c679a523cbab67
Mohamed fareed <131618380+mfc0d1ng@users.noreply.github.com>
2023-06-08 10:33:34 +0300

    Update make.cpp

--- make/make.cpp
+++ make/make.cpp
@@ -55,13 +55,6 @@ void open_map_file(const char *argv1, FILE **map)
 }
 
 
-void clean_up(const char *__ptr, FILE **map)
-{
-    std::printf("Invalid option ==> (%s) ... program terminated.\n", __ptr); 
-    clean_up(map);
-}
-
-
 void clean_up(FILE **map)
 {
     FILE **__tmp = map;
